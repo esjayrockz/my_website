0e2ab25e5062fe231b52f066c931c41d41765483
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-01-08 09:31:39 -0500

    Minor changes to views,controllers and stylesheets

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
 before_action :find_project, only: [:show, :edit, :update, :destroy]
 
   def index
-    @projects = Project.all.order("created_at").paginate(page: params[:page], per_page: 5)
+    @projects = Project.all.order("created_at").paginate(page: params[:page], per_page: 10)
   end
 
   def new
